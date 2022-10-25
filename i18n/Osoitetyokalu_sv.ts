063f7ee7391c1f878d0d62189b79b3310a501371
Valtteri Lappalainen <valtteri.lappalainen@sitowise.com>
2022-10-25 14:01:52 +0300

    Initial support for translations

---
+++ i18n/Osoitetyokalu_sv.ts
@@ -0,0 +1,323 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="2.0" language="sv" sourcelanguage="fi">
+<context>
+    <name>Center</name>
+    <message>
+        <location filename="../dialogs/SearchForm_dialog.ui" line="209"/>
+        <source>Kohdista</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../dialogs/SearchForm_dialog.ui" line="37"/>
+        <source>X:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../dialogs/SearchForm_dialog.ui" line="49"/>
+        <source>Y:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../dialogs/SearchForm_dialog.ui" line="61"/>
+        <source>Tie:</source>
+        <translation>Väg:</translation>
+    </message>
+    <message>
+        <location filename="../dialogs/SearchForm_dialog.ui" line="73"/>
+        <source>Ajorata:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../dialogs/SearchForm_dialog.ui" line="85"/>
+        <source>Osa:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../dialogs/SearchForm_dialog.ui" line="97"/>
+        <source>Et&#xc3;&#xa4;isyys:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../dialogs/SearchForm_dialog.ui" line="230"/>
+        <source>Osa_loppu:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../dialogs/SearchForm_dialog.ui" line="242"/>
+        <source>Et&#xc3;&#xa4;isyys_loppu:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../dialogs/SearchForm_dialog.ui" line="266"/>
+        <source>Kuntanimi:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../dialogs/SearchForm_dialog.ui" line="278"/>
+        <source>Katunimi:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../dialogs/SearchForm_dialog.ui" line="290"/>
+        <source>Katunumero:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../dialogs/SearchForm_dialog.ui" line="327"/>
+        <source>Kmtk_id:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../dialogs/SearchForm_dialog.ui" line="339"/>
+        <source>M-arvo:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../dialogs/SearchForm_dialog.ui" line="455"/>
+        <source>Sulje</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../dialogs/SearchForm_dialog.ui" line="468"/>
+        <source>Koko tien tai useiden tieosien hakeminen voi kest&#xc3;&#xa4;&#xc3;&#xa4; useita minuutteja.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../dialogs/SearchForm_dialog.ui" line="481"/>
+        <source>(24.10.2022)
+Kmtk_id- ja m-arvo-parametrit tulevat k&#xc3;&#xa4;ytt&#xc3;&#xb6;&#xc3;&#xb6;n
+VKM:n seuraavassa p&#xc3;&#xa4;ivityksess&#xc3;&#xa4;.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Delete</name>
+    <message>
+        <location filename="../dialogs/DeleteLayer_dialog.ui" line="14"/>
+        <source>Poistoty&#xc3;&#xb6;kalu</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../dialogs/DeleteLayer_dialog.ui" line="26"/>
+        <source>Poista kaikki karttavihjeet</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../dialogs/DeleteLayer_dialog.ui" line="39"/>
+        <source>Poista yksi karttavihje</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../dialogs/DeleteLayer_dialog.ui" line="52"/>
+        <source>Poista yksi geometria</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../dialogs/DeleteLayer_dialog.ui" line="65"/>
+        <source>Poista kaikki geometriat</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Osoitetyokalu</name>
+    <message>
+        <location filename="../Osoitetyokalu.py" line="276"/>
+        <source>&amp;Osoitetyokalu</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Osoitetyokalu.py" line="214"/>
+        <source>1. Tieosoite</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Osoitetyokalu.py" line="223"/>
+        <source>2. Hakuty&#xc3;&#xb6;kalu</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Osoitetyokalu.py" line="231"/>
+        <source>3. Tieosa</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Osoitetyokalu.py" line="239"/>
+        <source>4. Tieosoite (Alku- ja loppupiste)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Osoitetyokalu.py" line="247"/>
+        <source>5. Kohdistusty&#xc3;&#xb6;kalu</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Osoitetyokalu.py" line="255"/>
+        <source>6. Poistoty&#xc3;&#xb6;kalu</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Properties</name>
+    <message>
+        <location filename="../dialogs/PopUp_dialog.ui" line="20"/>
+        <source>Ominaisuudet</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../dialogs/PopUp_dialog.ui" line="305"/>
+        <source>Sulje</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../dialogs/PopUp_dialog.ui" line="331"/>
+        <source>X:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../dialogs/PopUp_dialog.ui" line="343"/>
+        <source>Y:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../dialogs/PopUp_dialog.ui" line="355"/>
+        <source>Tie:</source>
+        <translation type="unfinished">Väg:</translation>
+    </message>
+    <message>
+        <location filename="../dialogs/PopUp_dialog.ui" line="367"/>
+        <source>Ajorata:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../dialogs/PopUp_dialog.ui" line="379"/>
+        <source>Osa:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../dialogs/PopUp_dialog.ui" line="391"/>
+        <source>Etaisyys:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../dialogs/PopUp_dialog.ui" line="403"/>
+        <source>Hallinnollinen luokka:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../dialogs/PopUp_dialog.ui" line="647"/>
+        <source>Kuntanimi:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../dialogs/PopUp_dialog.ui" line="659"/>
+        <source>Katunimi:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../dialogs/PopUp_dialog.ui" line="671"/>
+        <source>Katunumero:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../dialogs/PopUp_dialog.ui" line="683"/>
+        <source>Elynimi:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../dialogs/PopUp_dialog.ui" line="695"/>
+        <source>Ualuenimi:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../dialogs/PopUp_dialog.ui" line="707"/>
+        <source>Maakuntanimi:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../dialogs/PopUp_dialog.ui" line="807"/>
+        <source>M-arvo:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../dialogs/PopUp_dialog.ui" line="825"/>
+        <source>Kmtk_id:</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Roadways</name>
+    <message>
+        <location filename="../dialogs/Ajoradat_dialog.ui" line="14"/>
+        <source>Ajoradat</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../dialogs/Ajoradat_dialog.ui" line="29"/>
+        <source>Ajorata 0 (vihre&#xc3;&#xa4;):</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../dialogs/Ajoradat_dialog.ui" line="36"/>
+        <source>Ajorata 1 (punainen):</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../dialogs/Ajoradat_dialog.ui" line="43"/>
+        <source>Ajorata 2 (sininen):</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../dialogs/Ajoradat_dialog.ui" line="50"/>
+        <source>Tiepituus:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../dialogs/Ajoradat_dialog.ui" line="57"/>
+        <source>Ajoratojen kokonaispituus:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../dialogs/Ajoradat_dialog.ui" line="103"/>
+        <source>Lataa v&#xc3;&#xa4;lin ajoratojen osoitetiedot</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../dialogs/Ajoradat_dialog.ui" line="119"/>
+        <source>Sijainti:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../dialogs/Ajoradat_dialog.ui" line="133"/>
+        <source>...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../dialogs/Ajoradat_dialog.ui" line="149"/>
+        <source>Sulje</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Tieosoite</name>
+    <message>
+        <location filename="../dialogs/ShowCoordinates_dialog.ui" line="14"/>
+        <source>Tieosoite</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../dialogs/ShowCoordinates_dialog.ui" line="31"/>
+        <source>Koordinaatit:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../dialogs/ShowCoordinates_dialog.ui" line="54"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:10pt;&quot;&gt;Tieosoite:&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../dialogs/ShowCoordinates_dialog.ui" line="77"/>
+        <source>Sulje</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
