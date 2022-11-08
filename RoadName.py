b025915b139536df701bfa48e70781572ee3cd16
Aleksandr <aleksandr.roos@sitowise.com>
2022-11-08 15:00:17 +0200

    road name, reformatted readme, new main icon

--- RoadName.py
+++ RoadName.py
@@ -1,16 +1,28 @@
+import contextlib
+import json
 from pathlib import Path
 
 
 class RoadName:
-    
+
 
     def __init__(self) -> None:
-        path = Path('data/tienimi.json')
+        path = Path(__file__).parent / "data/tienimi.json"
         self.file_path = path.resolve()
 
 
     def get_road_name(self, road_number:int):
+        """Retrieves a road name based on the given road number.
+
+        Args:
+            road_number (int): Road number.
+
+        Returns:
+            (str): Road name.
+        """
         with open(self.file_path, 'r') as file:
-            for row in file:
-                if row['tie'] == road_number:
-                    return row['tienimi']
+            data = json.load(file)
+        for road in data:
+            with contextlib.suppress(KeyError):
+                if road['tie'] == road_number:
+                    return road['tienimi']
