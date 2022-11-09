28eb6a689d41f3fadf3301c14afa0fa711310785
Aleksandr <aleksandr.roos@sitowise.com>
2022-11-09 15:48:53 +0200

    added license, renamed plugin

--- LayerHandler.py
+++ LayerHandler.py
@@ -1,4 +1,21 @@
-from abc import abstractstaticmethod
+"""
+/*
+
+* Copyright 2022 Finnish Transport Infrastructure Agency
+*
+
+* Licensed under the EUPL, Version 1.2 or – as soon they will be approved by the European Commission - subsequent versions of the EUPL (the "Licence");
+* You may not use this work except in compliance with the Licence.
+* You may obtain a copy of the Licence at:
+*
+* https://joinup.ec.europa.eu/sites/default/files/custom-page/attachment/2020-03/EUPL-1.2%20EN.txt
+*
+* Unless required by applicable law or agreed to in writing, software distributed under the Licence is distributed on an "AS IS" basis,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the Licence for the specific language governing permissions and limitations under the Licence.
+*/
+"""
+
 
 from qgis.core import (QgsCoordinateReferenceSystem, QgsFeature, QgsField,
                        QgsGeometry, QgsMarkerSymbol, QgsPointXY, QgsProject,
@@ -81,9 +98,9 @@ class LayerHandler(object):
         Args:
             group_name (str): Name of the group.
         """
-        tool_group = self.root.findGroup(self.tr(u'Osoitetyökalu'))
+        tool_group = self.root.findGroup(self.tr(u'Tieosoitetyökalu'))
         if tool_group is None:
-            tool_group = self.root.insertGroup(0, self.tr(u'Osoitetyökalu'))
+            tool_group = self.root.insertGroup(0, self.tr(u'Tieosoitetyökalu'))
         group = tool_group.findGroup(group_name)
 
         return tool_group.addGroup(group_name) if group is None else group
--- i18n/Osoitetyokalu_en.ts
+++ i18n/Osoitetyokalu_en.ts
@@ -3,7 +3,7 @@
 <context>
     <name>Ajoradat_dialog</name>
     <message>
-        <location filename="../dialogs/Ajoradat_dialog.py" line="40"/>
+        <location filename="../dialogs/Ajoradat_dialog.py" line="59"/>
         <source>Valitse tallennussijainti</source>
         <translation>Choose a download folder</translation>
     </message>
@@ -115,116 +115,111 @@ in the next VKM API update.</translation>
 </context>
 <context>
     <name>LayerHandler</name>
-    <message encoding="UTF-8">
-        <location filename="../LayerHandler.py" line="86"/>
-        <source>Osoitetyökalu</source>
-        <translation>Road address tool</translation>
-    </message>
     <message>
-        <location filename="../LayerHandler.py" line="93"/>
+        <location filename="../LayerHandler.py" line="110"/>
         <source>1. Tieosoite</source>
         <translation>1. Road address</translation>
     </message>
     <message encoding="UTF-8">
-        <location filename="../LayerHandler.py" line="100"/>
+        <location filename="../LayerHandler.py" line="117"/>
         <source>2. Hakutyökalu</source>
         <translation>2. Search tool</translation>
     </message>
     <message>
-        <location filename="../LayerHandler.py" line="107"/>
+        <location filename="../LayerHandler.py" line="124"/>
         <source>3. Tieosa</source>
         <translation>3. Road part</translation>
     </message>
     <message>
-        <location filename="../LayerHandler.py" line="127"/>
+        <location filename="../LayerHandler.py" line="144"/>
         <source>4. Tieosoite (Alku- ja loppupiste)</source>
         <translation>4. Road address (Starting and ending point)</translation>
     </message>
     <message encoding="UTF-8">
-        <location filename="../LayerHandler.py" line="141"/>
+        <location filename="../LayerHandler.py" line="158"/>
         <source>5. Kohdistustyökalu</source>
         <translation>5. Centering tool</translation>
     </message>
     <message>
-        <location filename="../LayerHandler.py" line="292"/>
+        <location filename="../LayerHandler.py" line="309"/>
         <source>Ajoradat 0</source>
         <translation>Roadways 0</translation>
     </message>
     <message>
-        <location filename="../LayerHandler.py" line="311"/>
+        <location filename="../LayerHandler.py" line="328"/>
         <source>Ajoradat 1</source>
         <translation>Roadways 1</translation>
     </message>
     <message>
-        <location filename="../LayerHandler.py" line="330"/>
+        <location filename="../LayerHandler.py" line="347"/>
         <source>Ajoradat 2</source>
         <translation>Roadways 2</translation>
     </message>
     <message>
-        <location filename="../LayerHandler.py" line="380"/>
+        <location filename="../LayerHandler.py" line="397"/>
         <source>Karttavihjeet</source>
         <translation>Annotations</translation>
     </message>
     <message>
-        <location filename="../LayerHandler.py" line="382"/>
+        <location filename="../LayerHandler.py" line="399"/>
         <source>Pisteet</source>
         <translation>Points</translation>
     </message>
     <message>
-        <location filename="../LayerHandler.py" line="384"/>
+        <location filename="../LayerHandler.py" line="401"/>
         <source>Alkupisteet</source>
         <translation>Starting points</translation>
     </message>
     <message>
-        <location filename="../LayerHandler.py" line="386"/>
+        <location filename="../LayerHandler.py" line="403"/>
         <source>Loppupisteet</source>
         <translation>Ending points</translation>
     </message>
+    <message encoding="UTF-8">
+        <location filename="../LayerHandler.py" line="103"/>
+        <source>Tieosoitetyökalu</source>
+        <translation>Road address tool</translation>
+    </message>
 </context>
-<context>
+<context encoding="UTF-8">
     <name>Osoitetyokalu</name>
     <message>
-        <location filename="../Osoitetyokalu.py" line="277"/>
-        <source>&amp;Osoitetyokalu</source>
-        <translation>&amp;Road address tool</translation>
-    </message>
-    <message>
-        <location filename="../Osoitetyokalu.py" line="215"/>
+        <location filename="../Osoitetyokalu.py" line="246"/>
         <source>1. Tieosoite</source>
         <translation>1. Road address</translation>
     </message>
     <message encoding="UTF-8">
-        <location filename="../Osoitetyokalu.py" line="224"/>
+        <location filename="../Osoitetyokalu.py" line="255"/>
         <source>2. Hakutyökalu</source>
         <translation>2. Search tool</translation>
     </message>
     <message>
-        <location filename="../Osoitetyokalu.py" line="232"/>
+        <location filename="../Osoitetyokalu.py" line="263"/>
         <source>3. Tieosa</source>
         <translation>3. Road part</translation>
     </message>
     <message>
-        <location filename="../Osoitetyokalu.py" line="240"/>
+        <location filename="../Osoitetyokalu.py" line="271"/>
         <source>4. Tieosoite (Alku- ja loppupiste)</source>
         <translation>4. Road address (Starting and ending point)</translation>
     </message>
     <message encoding="UTF-8">
-        <location filename="../Osoitetyokalu.py" line="248"/>
+        <location filename="../Osoitetyokalu.py" line="279"/>
         <source>5. Kohdistustyökalu</source>
         <translation>5. Centering tool</translation>
     </message>
     <message encoding="UTF-8">
-        <location filename="../Osoitetyokalu.py" line="256"/>
+        <location filename="../Osoitetyokalu.py" line="287"/>
         <source>6. Poistotyökalu</source>
         <translation>6. Delete tool</translation>
     </message>
     <message encoding="UTF-8">
-        <location filename="../Osoitetyokalu.py" line="691"/>
+        <location filename="../Osoitetyokalu.py" line="722"/>
         <source>Pistettä ei ole asetettu.</source>
         <translation>Point is not set.</translation>
     </message>
     <message>
-        <location filename="../Osoitetyokalu.py" line="478"/>
+        <location filename="../Osoitetyokalu.py" line="509"/>
         <source>Alkupiste: {start}
 Loppupiste: {end}
 pituus: {length}m</source>
@@ -233,64 +228,69 @@ Ending point: {end}
 length: {length}m</translation>
     </message>
     <message encoding="UTF-8">
-        <location filename="../Osoitetyokalu.py" line="625"/>
+        <location filename="../Osoitetyokalu.py" line="656"/>
         <source>Alku- ja loppupisteen on oltava samalla tiellä</source>
         <translation>Starting and ending points have to be on the same road</translation>
     </message>
     <message>
-        <location filename="../Osoitetyokalu.py" line="645"/>
+        <location filename="../Osoitetyokalu.py" line="676"/>
         <source>A {tie_A}/{ajorata_A}/{osa_A}/{etaisyys_A} - B {tie_B}/{ajorata_B}/{osa_B}/{etaisyys_B} pituus: {mitattu_pituus}</source>
         <translation>A {tie_A}/{ajorata_A}/{osa_A}/{etaisyys_A} - B {tie_B}/{ajorata_B}/{osa_B}/{etaisyys_B} length: {mitattu_pituus}</translation>
     </message>
     <message>
-        <location filename="../Osoitetyokalu.py" line="813"/>
+        <location filename="../Osoitetyokalu.py" line="844"/>
         <source>Ei tieosoitetta</source>
         <translation>No road address</translation>
     </message>
     <message encoding="UTF-8">
-        <location filename="../Osoitetyokalu.py" line="1100"/>
+        <location filename="../Osoitetyokalu.py" line="1131"/>
         <source>Täytä vaaditut kentät.</source>
         <translation>Fill out necessary fields.</translation>
     </message>
     <message>
-        <location filename="../Osoitetyokalu.py" line="1199"/>
+        <location filename="../Osoitetyokalu.py" line="1230"/>
         <source>VKM-API ei vastaa. URL: {final_url}</source>
         <translation>VKM API does not respond. URL: {final_url}</translation>
     </message>
     <message encoding="UTF-8">
-        <location filename="../Osoitetyokalu.py" line="1254"/>
+        <location filename="../Osoitetyokalu.py" line="1285"/>
         <source>Pistemäinen haku</source>
         <translation>Point-like search</translation>
     </message>
     <message>
-        <location filename="../Osoitetyokalu.py" line="1402"/>
+        <location filename="../Osoitetyokalu.py" line="1433"/>
         <source>Alkupiste {road}/{roadway}/{part}/{distance} - Loppupiste {road_end}/{roadway_end}/{part_end}/{distance_end}, mitattu pituus: {measured_length}</source>
         <translation>Starting point {road}/{roadway}/{part}/{distance} - Ending point {road_end}/{roadway_end}/{part_end}/{distance_end}, measured_length: {measured_length}</translation>
     </message>
     <message>
-        <location filename="../Osoitetyokalu.py" line="1432"/>
+        <location filename="../Osoitetyokalu.py" line="1463"/>
         <source>Alkupiste</source>
         <translation>Starting point</translation>
     </message>
     <message>
-        <location filename="../Osoitetyokalu.py" line="1436"/>
+        <location filename="../Osoitetyokalu.py" line="1467"/>
         <source>Loppupiste</source>
         <translation>Ending point</translation>
     </message>
     <message>
-        <location filename="../Osoitetyokalu.py" line="1527"/>
+        <location filename="../Osoitetyokalu.py" line="1543"/>
         <source>Tiedosto tallennettu polkuun: {user_path}</source>
         <translation>File saved in:</translation>
     </message>
     <message>
-        <location filename="../Osoitetyokalu.py" line="1528"/>
+        <location filename="../Osoitetyokalu.py" line="1544"/>
         <source>Lataus onnistui</source>
         <translation>Download succeeded</translation>
     </message>
     <message encoding="UTF-8">
-        <location filename="../Osoitetyokalu.py" line="1535"/>
+        <location filename="../Osoitetyokalu.py" line="1551"/>
         <source>Virhe tiedostoa ladattaessa. Yritä uudelleen.</source>
         <translation>Error while downloading the file. Try again.</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="../Osoitetyokalu.py" line="308"/>
+        <source>&amp;Tieosoitetyökalu</source>
+        <translation>Road address tool</translation>
     </message>
 </context>
 <context>
@@ -331,39 +331,44 @@ length: {length}m</translation>
         <translation>Administrative class:</translation>
     </message>
     <message>
-        <location filename="../dialogs/PopUp_dialog.ui" line="647"/>
+        <location filename="../dialogs/PopUp_dialog.ui" line="659"/>
         <source>Kuntanimi:</source>
         <translation>Municipality name:</translation>
     </message>
     <message>
-        <location filename="../dialogs/PopUp_dialog.ui" line="659"/>
+        <location filename="../dialogs/PopUp_dialog.ui" line="671"/>
         <source>Katunimi:</source>
         <translation>Street name:</translation>
     </message>
     <message>
-        <location filename="../dialogs/PopUp_dialog.ui" line="671"/>
+        <location filename="../dialogs/PopUp_dialog.ui" line="683"/>
         <source>Katunumero:</source>
         <translation>Street number:</translation>
     </message>
     <message>
-        <location filename="../dialogs/PopUp_dialog.ui" line="683"/>
+        <location filename="../dialogs/PopUp_dialog.ui" line="695"/>
         <source>Elynimi:</source>
         <translation>ETE centre:</translation>
     </message>
     <message>
-        <location filename="../dialogs/PopUp_dialog.ui" line="695"/>
+        <location filename="../dialogs/PopUp_dialog.ui" line="707"/>
         <source>Ualuenimi:</source>
         <translation>Maintenance area:</translation>
     </message>
     <message>
-        <location filename="../dialogs/PopUp_dialog.ui" line="707"/>
+        <location filename="../dialogs/PopUp_dialog.ui" line="719"/>
         <source>Maakuntanimi:</source>
         <translation>Province name:</translation>
     </message>
     <message>
-        <location filename="../dialogs/PopUp_dialog.ui" line="807"/>
+        <location filename="../dialogs/PopUp_dialog.ui" line="831"/>
         <source>M-arvo:</source>
         <translation>M-value:</translation>
+    </message>
+    <message>
+        <location filename="../dialogs/PopUp_dialog.ui" line="731"/>
+        <source>Maantien nimi:</source>
+        <translation>Road name:</translation>
     </message>
 </context>
 <context>
@@ -386,7 +391,7 @@ length: {length}m</translation>
     <message>
         <location filename="../dialogs/Ajoradat_dialog.ui" line="43"/>
         <source>Ajorata 2 (sininen):</source>
-        <translation>Roadway 2 (blue)</translation>
+        <translation>Roadway 2 (blue):</translation>
     </message>
     <message>
         <location filename="../dialogs/Ajoradat_dialog.ui" line="50"/>
@@ -445,7 +450,7 @@ length: {length}m</translation>
 <context encoding="UTF-8">
     <name>VkmApiException</name>
     <message encoding="UTF-8">
-        <location filename="../CustomExceptions/VkmApiException.py" line="6"/>
+        <location filename="../CustomExceptions/VkmApiException.py" line="25"/>
         <source>VKM-rajapintaan ei saada yhteyttä.</source>
         <translation>Cannot connect to VKM API.</translation>
     </message>
@@ -453,7 +458,7 @@ length: {length}m</translation>
 <context>
     <name>VkmRequestException</name>
     <message>
-        <location filename="../CustomExceptions/VkmRequestException.py" line="7"/>
+        <location filename="../CustomExceptions/VkmRequestException.py" line="26"/>
         <source>VKM-API virhe: </source>
         <translation>VKM API error:</translation>
     </message>
